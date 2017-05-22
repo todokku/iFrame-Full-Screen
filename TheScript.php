e02b099e50dda4f18397d11da0aebe79a542e14c
mnuradli1 <mnuradli1@gmail.com>
2017-05-22 22:55:56 +0700

    Update TheScript.php

--- TheScript.php
+++ TheScript.php
@@ -1,6 +1,6 @@
 <head>
 <title>
-Domus Amoris
+Web Title
 </title>
 <style>
 body {
@@ -9,7 +9,7 @@ body {
 </style>
 </head>
 <body>
-<iframe src="http://vimaned.jatayuweb.com" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+<iframe src="http://yourlinkhere.com" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
     Your browser doesn't support iframes
 </iframe>
 </body>
