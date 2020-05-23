665375a3b686691e99ec4f99f4ddc2652f6a15d9
todokku <59433408+todokku@users.noreply.github.com>
2020-05-23 22:46:39 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>
-فورتنایت
+کرانکر
 </title>
 <style>
 body {
@@ -10,7 +10,7 @@ body {
 </style>
 </head>
 <body>
-<iframe src="https://miniroyale2.io/" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+<iframe src="https://krunker.io/" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
     Your browser doesn't support iframes
 </iframe>
 </body>
